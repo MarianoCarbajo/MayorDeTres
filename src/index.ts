a50f575dc523f1edea32fd35b627fd73f547f26a
Mariano Carbajo <104107401+cobacho666@users.noreply.github.com>
2022-05-02 15:43:15 -0300

    Rotulo4 Se agrego el rotulo 4

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ let btnEnviar = document.getElementById("enviar");
 let rotulo1 = document.getElementById("rotulo1");
 let rotulo2 = document.getElementById("rotulo2");
 let rotulo3 = document.getElementById("rotulo3");
+let rotulo4 = document.getElementById("rotulo4");
 
 rotulo1.innerHTML = "Ingrese el primer número";
 rotulo2.innerHTML = "Ingrese el segundo número";
@@ -17,12 +18,16 @@ btnEnviar.addEventListener("click", () => {
   let numero3: number = Number(dato3.value);
 
   if (numero1 == numero2 || numero1 == numero3 || numero2 == numero3) {
+    rotulo4.innerHTML = "Los números deben ser diferentes";
     console.log("Los números deben ser diferentes");
   } else if (numero1 > numero2 && numero1 > numero3) {
+    rotulo4.innerHTML = "El mayor de los tres es: " + numero1;
     console.log("El mayor de los tres es: ", numero1);
   } else if (numero1 < numero2 && numero2 > numero3) {
+    rotulo4.innerHTML = "El mayor de los tres es: " + numero2;
     console.log("El mayor de los tres es: ", numero2);
   } else {
+    rotulo4.innerHTML = "El mayor de los tres es: " + numero3;
     console.log("El mayor de los tres es: ", numero3);
   }
 });
